f554ccda5eba2430b5ca36c19fc1580e2fd406dc
Gareth Brown <garethbrown82@gmail.com>
2019-02-05 06:28:34 +0000

    Create tests for all six levels
    To see if they can be completed using the correct number combintation

--- src/gameMechanics/levels.test.js
+++ src/gameMechanics/levels.test.js
@@ -26,4 +26,112 @@ describe('Game component', () => {
 
     expect(gameWrapper.state().isLevelCompleted).toBe(true)
   })
+
+  it('completes level two with the correct button sequence', async () => {
+    const gameWrapper = shallow(<Game />)
+    const instance = gameWrapper.instance()
+    await instance.initialiseGame()
+    instance.resetLevel(1)
+    expect(gameWrapper.state().levelIndex).toBe(1)
+
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.BottomLeft)
+    instance.triggerButton(Button.BottomLeft)
+    instance.triggerButton(Button.BottomRight)
+    instance.triggerButton(Button.BottomRight)
+
+    expect(gameWrapper.state().isLevelCompleted).toBe(true)
+  })
+
+  it('completes level three with the correct button sequence', async () => {
+    const gameWrapper = shallow(<Game />)
+    const instance = gameWrapper.instance()
+    await instance.initialiseGame()
+    instance.resetLevel(2)
+    expect(gameWrapper.state().levelIndex).toBe(2)
+
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.BottomRight)
+    instance.triggerButton(Button.BottomRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.BottomLeft)
+    instance.triggerButton(Button.BottomLeft)
+
+    expect(gameWrapper.state().isLevelCompleted).toBe(true)
+  })
+
+  it('completes level four with the correct button sequence', async () => {
+    const gameWrapper = shallow(<Game />)
+    const instance = gameWrapper.instance()
+    await instance.initialiseGame()
+    instance.resetLevel(3)
+    expect(gameWrapper.state().levelIndex).toBe(3)
+
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.BottomRight)
+    instance.triggerButton(Button.BottomLeft)
+
+    expect(gameWrapper.state().isLevelCompleted).toBe(true)
+  })
+
+  it('completes level five with the correct button sequence', async () => {
+    const gameWrapper = shallow(<Game />)
+    const instance = gameWrapper.instance()
+    await instance.initialiseGame()
+    instance.resetLevel(4)
+    expect(gameWrapper.state().levelIndex).toBe(4)
+
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.BottomLeft)
+    instance.triggerButton(Button.BottomLeft)
+    instance.triggerButton(Button.BottomRight)
+
+    expect(gameWrapper.state().isLevelCompleted).toBe(true)
+  })
+
+  it('completes level six with the correct button sequence', async () => {
+    const gameWrapper = shallow(<Game />)
+    const instance = gameWrapper.instance()
+    await instance.initialiseGame()
+    instance.resetLevel(5)
+    expect(gameWrapper.state().levelIndex).toBe(5)
+
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopLeft)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.TopRight)
+    instance.triggerButton(Button.BottomRight)
+    instance.triggerButton(Button.BottomLeft)
+    instance.triggerButton(Button.BottomLeft)
+
+    expect(gameWrapper.state().isLevelCompleted).toBe(true)
+  })
 })
